69b0a88586acddc0907f3f76ae9f7abc8a08453b
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-02-21 10:43:59 -0500

    full name generator

--- src/game_info.rs
+++ src/game_info.rs
@@ -1,18 +1,47 @@
 use std::{collections::HashMap};
 
-use rand::Rng;
+use rand::{Rng};
 
 #[derive(Clone)]
 pub struct Player {
-    pub team: u64,
     pub id: u64,
+    pub team: u64,
     pub name: String,
     pub strength: f32,
     pub speed: f32,
 }
 
+#[derive(Clone)]
+pub struct Team {
+    pub id: u64,
+    pub icon: char,
+    pub name: String,
+    pub location: String,
+    pub players: Vec<u64>,
+}
+
+
+pub fn generate_name(name_list: &Vec<&str>, surname_list: &Vec<&str>, general_list: &Vec<&str>) -> String {
+    let index_name = rand::thread_rng().gen_range(0..(name_list.len() + general_list.len()));
+    let index_surname = rand::thread_rng().gen_range(0..(surname_list.len() + general_list.len()));
+    let mut _name = "".to_string();
+    if index_name >= name_list.len() {
+        _name += general_list[index_name - name_list.len()];
+    } else {
+        _name += name_list[index_name];
+    }
+    _name += " ";
+    if index_surname >= surname_list.len() {
+        _name += general_list[index_surname - surname_list.len()];
+    } else {
+        _name += surname_list[index_surname];
+    }
+    // println!("{}", _name);
+    _name
+}
+
 impl Player {
-    pub fn new(players: &mut HashMap<u64, Player>) -> Player {
+    pub fn new(players: &mut HashMap<u64, Player>, teams: &mut HashMap<u64, Team>, team: u64) -> u64 {
         let id: u64 = rand::thread_rng().gen();
         let p = Player {
             id: id,
@@ -22,6 +51,30 @@ impl Player {
             speed: 6.5,
         };
         players.insert(id, p.clone());
-        p.clone()
+
+        if let Some(value) = teams.get_mut(&team) {
+            value.players.push(id);
+        }
+
+        id
+    }
+}
+
+impl Team {
+    pub fn new(teams: &mut HashMap<u64, Team>, players: &mut HashMap<u64, Player>, name: String, icon: char, location: String) -> u64 {
+        let id: u64 = rand::thread_rng().gen();
+        let t = Team {
+            id: id,
+            name: name,
+            icon: icon,
+            location: location,
+            players: Vec::new(),
+        };
+        teams.insert(id, t.clone());
+        for _ in 0..12 {
+            Player::new(players, teams, id);
+        }
+
+        id
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,32 @@
 mod game_info;
 
-use std::{/*io,*/ collections::HashMap};
+use std::{/*io,*/ collections::HashMap, fs};
 // use console::Term;
+pub use game_info::*;
 pub use game_info::Player;
-
-#[derive(Clone)]
-struct Team {
-    id: u64,
-    name: String,
-    location: String,
-    players: Vec<Player>,
-}
-
+pub use game_info::Team;
 
 
 fn main() {
-    let mut _teams: HashMap<u64, Team> = HashMap::new();
+
+    let contents = fs::read_to_string("names.txt").expect("Something went wrong with names.txt");
+    let names: Vec<&str> = contents.split("\r\n").collect();
+
+    let contents = fs::read_to_string("surnames.txt").expect("Something went wrong with surnames.txt");
+    let last_names: Vec<&str> = contents.split("\r\n").collect();
+
+    let contents = fs::read_to_string("general.txt").expect("Something went wrong with general.txt");
+    let general_names: Vec<&str> = contents.split("\r\n").collect();
+
+    drop(&contents);
+
+
+    let mut teams: HashMap<u64, Team> = HashMap::new();
     let mut players :HashMap<u64, Player>= HashMap::new();
-    Player::new(&mut players);
-    for (id, player) in players {
-        println!("Player {} has name {}", id, player.name);
+    generate_name(&names, &last_names, &general_names);
+    let _t = Team::new(&mut teams, &mut players, "uwu".to_string(), 'a', "a".to_string());
+    for _ in 0..12 {
+        let name = generate_name(&names, &last_names, &general_names);
+        println!("{}", name);
     }
-    
 }
